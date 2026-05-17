fb5499194d8d17faa82d6ab5b55db851855e5be0
Deemoun <deemoun@users.noreply.github.com>
2026-05-16 22:27:54 -0700

    Modifying the test

--- app/src/test/java/com/training/vulnerablebank/MainAppTest.java
+++ app/src/test/java/com/training/vulnerablebank/MainAppTest.java
@@ -1,5 +1,6 @@
 package com.training.vulnerablebank;
 import static com.training.vulnerablebank.utils.TestAssertions.assertTextEqualsAny;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Disabled;
